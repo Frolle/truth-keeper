46c72aba490b3b23b6a09274101a3c2a4dfbeb77
Simon Dirnberger <simon.dirnberger@gu.se>
2024-10-13 14:53:16 +0200

    Initial commit

---
+++ src/main/java/se/dirnberger/truthsayer_backend/TruthsayerBackendApplication.java
@@ -0,0 +1,13 @@
+package se.dirnberger.truthsayer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruthsayerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruthsayerBackendApplication.class, args);
+	}
+
+}
